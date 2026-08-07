7c442cf3e34df697bd8a2692101a72fd243da569
Rupchand Naiya <rupchandnaiya27@gmail.com>
2026-08-07 13:49:28 +0530

    Make Website Reponsive

--- init/index.js
+++ init/index.js
@@ -3,7 +3,7 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect('mongodb://127.0.0.1:27017/rentora');
 }
 
 main()
